743fe0e871668b35052074d92aa4270237f761ff
dakota-hawkins <dyh0110@bu.edu>
2021-06-28 23:48:34 -0400

    download data using scanpy.

---
+++ scripts/download_data.py
@@ -0,0 +1,20 @@
+import scanpy as sc
+
+def download_data(dataset):
+    if dataset == 'pbmc3k':
+        adata = scanpy.datasets.pbmc3k()
+    elif dataset == 'paul':
+        adata = scanpy.datasets.paul15()
+    elif dataset == 'moignard':
+        adata = scanpy.datasets.moignard15()
+    else:
+        raise ValueError(f"Unsupported dataset {dataset}.")
+
+if __name__ == '__main__':
+    try:
+        snakemake
+    except NameError:
+        snakemake = None
+    if snakemake is not None:
+        adata = download_data(snakemake.params['dataset'])
+        adata.write(snakemake.output)
