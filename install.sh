859061ac6ec992c7a4d4ba92e873ed67ab5d22c1
dakota-hawkins <dyh0110@bu.edu>
2021-06-29 10:27:51 -0400

    added igraph

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 module load miniconda
 conda create --name workshop seaborn scikit-learn statsmodels numba pytables
 conda activate workshop
-conda install -c conda-forge python-igraph leidenalg
+conda install -c conda-forge igraph python-igraph leidenalg
 pip install scanpy snakemake scikit-misc
