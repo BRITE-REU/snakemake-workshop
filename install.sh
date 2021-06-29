af06ac4779030071442b92ae4a68f353615f428b
dakota-hawkins <dyh0110@bu.edu>
2021-06-29 12:26:40 -0400

    install louvain via pip

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ module load miniconda
 conda create --name workshop seaborn scikit-learn statsmodels numba pytables
 conda activate workshop
 conda install -c conda-forge igraph python-igraph leidenalg
-pip install scanpy snakemake scikit-misc
+pip install scanpy snakemake scikit-misc louvain
